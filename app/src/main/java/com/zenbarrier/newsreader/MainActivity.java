6744a180cae201c8bb3c86db96854a6e90a6555b
ZenBarrier <anthony.j.barrera@gmail.com>
2016-10-18 16:12:53 -0400

    Story IDs stored in database

--- app/src/main/java/com/zenbarrier/newsreader/MainActivity.java
+++ app/src/main/java/com/zenbarrier/newsreader/MainActivity.java
@@ -1,38 +1,47 @@
 package com.zenbarrier.newsreader;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
-import javax.net.ssl.HttpsURLConnection;
-
 public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase myDatabase;
+    ListView listView;
+    ArrayList<String> stories;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TaskGetJSON getJSON = new TaskGetJSON();
+        listView = (ListView) findViewById(R.id.listView);
+        stories = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, stories);
+        listView.setAdapter(adapter);
 
-        try {
-            String result = getJSON.execute("https://hacker-news.firebaseio.com/v0/item/12737326.json?print=pretty").get();
-            Log.i("result", result);
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
+        TaskGetStories getStories = new TaskGetStories();
+        myDatabase = this.openOrCreateDatabase("hackerNews",MODE_PRIVATE, null);
+        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS stories (id INTEGER, UNIQUE(id))");
+        getStories.execute("https://hacker-news.firebaseio.com/v0/newstories.json");
     }
 
-    class TaskGetJSON extends AsyncTask<String, Void, String>{
+    class TaskGetStories extends AsyncTask<String, Void, String>{
 
         @Override
         protected String doInBackground(String... urls) {
@@ -53,6 +62,23 @@ public class MainActivity extends AppCompatActivity {
             }
 
             return resultString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONArray array = new JSONArray(s);
+                for(int i = 0; i < array.length() ; i++){
+                    String sql = String.format("INSERT OR IGNORE INTO stories (id) VALUES (%s)", array.get(i).toString());
+                    myDatabase.execSQL(sql);
+                    stories.add(array.get(i).toString());
+                }
+                Log.i("done","done");
+                adapter.notifyDataSetChanged();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
