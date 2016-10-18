72ee526885b69548b1008e2b9d7eb774f1dde5f0
ZenBarrier <anthony.j.barrera@gmail.com>
2016-10-18 14:45:40 -0400

    Get JSON from url

--- app/src/main/java/com/zenbarrier/newsreader/MainActivity.java
+++ app/src/main/java/com/zenbarrier/newsreader/MainActivity.java
@@ -1,7 +1,14 @@
 package com.zenbarrier.newsreader;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    class TaskGetJSON extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String resultString = "";
+
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                int data = reader.read();
+                while(data != -1){
+                    resultString += (char)data;
+                    data = reader.read();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return resultString;
+        }
     }
 }
