14a62d4aa1c5ca240304d172c4ca274e6ab34416
ZenBarrier <anthony.j.barrera@gmail.com>
2016-10-18 14:55:20 -0400

    Use URLConnection instead of HttpURLConnection

--- app/src/main/java/com/zenbarrier/newsreader/MainActivity.java
+++ app/src/main/java/com/zenbarrier/newsreader/MainActivity.java
@@ -3,12 +3,17 @@ package com.zenbarrier.newsreader;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TaskGetJSON getJSON = new TaskGetJSON();
+
+        try {
+            String result = getJSON.execute("https://hacker-news.firebaseio.com/v0/item/12737326.json?print=pretty").get();
+            Log.i("result", result);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
     class TaskGetJSON extends AsyncTask<String, Void, String>{
@@ -27,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 URL url = new URL(urls[0]);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                URLConnection urlConnection = url.openConnection();
                 InputStream inputStream = urlConnection.getInputStream();
                 InputStreamReader reader = new InputStreamReader(inputStream);
                 int data = reader.read();
